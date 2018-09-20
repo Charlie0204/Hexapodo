eab638816fec34db650da6dab6a214ebdc3636af
Carlos Rondero <31300191+Charlie0204@users.noreply.github.com>
2018-09-19 17:32:42 -0700

    hexapodo archivos
    archivos

---
+++ w3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0409
+{
+    public partial class w3 : Form
+    {
+        public w3()
+        {
+            InitializeComponent();
+        }
+
+        private void pbxGoInfo_Click(object sender, EventArgs e)
+        {
+            Form2 windowData = new Form2();
+            windowData.Show();
+        }
+
+        private void pbxGoHome_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            w1 windowHome = new w1();
+            windowHome.Show();
+        }
+    }
+}
